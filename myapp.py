641ae7b0584dd1c44372a9b272b78feca3a8a9a1
Richard Teye Lawer <richardlawer@gmail.com>
2017-07-14 14:49:50 +0000

    yesss

--- myapp.py
+++ myapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,3 +12,8 @@ def whereami():
      return "Kdua"
 if __name__ == '__main__':
          app.run(host="0.0.0.0")
+
+
+@app.rout(name)
+def send_from(name):
+     return send_fcdrom_directory("index.thml",name)
