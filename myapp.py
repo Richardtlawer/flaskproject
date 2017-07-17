a25286efcc35f015b88cc4296a1c4f0da4c4a056
Richard Teye Lawer <richardlawer@gmail.com>
2017-07-17 11:59:37 +0000

    king Richard

--- myapp.py
+++ myapp.py
@@ -6,14 +6,38 @@ def index():
      return render_template("index.html")
 
 
-@app.route('/whereami')
 
+@app.route('/linus')
+def linux1():
+    return render_template("linux1.html")
+
+
+
+@app.route('/whereami')
 def whereami():
      return "Kdua"
 if __name__ == '__main__':
          app.run(host="0.0.0.0")
 
 
-@app.rout(name)
+
+#@app.route('/linus')
+#def linux1():
+ #  return render_template("linux1.html")
+
+
+@app.route('/name')
 def send_from(name):
      return send_fcdrom_directory("index.thml",name)
+
+
+
+#@app.route('/linus')
+#def linux1():
+ #  return render_template("linux1.html")
+
+
+
+
+
+
